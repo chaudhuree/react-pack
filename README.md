815558c36292b28cdb9b3b9da06917f015c72a3f
chaudhuree <chaudhuree00@gmail.com>
2022-06-30 07:25:03 +0600

    docs: added node-sass

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 | ----------------- | ----- | ----------- | --------- | ----- | ------- | ------------- | 
 | Installed Package | redux | react-redux | bootstrap | axios | graphql | apollo client |
 
-| 7                | 8                | 9             | 10     | 11       |        |
+| 7                | 8                | 9             | 10     | 11       | 12        |
 | ---------------- | ---------------- | ------------- | ------ | -------- | -------- |
-| styled component | react-router-dom | redux toolkit | recoil | firebase |  |
+| styled component | react-router-dom | redux toolkit | recoil | firebase | node-sass  |
--- src/App.js
+++ src/App.js
@@ -4,67 +4,73 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>React Pack</h1>
-      <p> -by chaudhuree</p>
-      <br/>
-      <br/>
-      <br/>
-      <hr />
-      <Table striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>Number</th>
-            <th>Package name</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>redux</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>react-redux</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>axios</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>graphql</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>apollo client</td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>bootstrap</td>
-          </tr>
-          <tr>
-            <td>7</td>
-            <td>react-router-dom</td>
-          </tr>
-          <tr>
-            <td>8</td>
-            <td>styled component</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>redux toolkit</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>recoil</td>
-          </tr>
-          <tr>
-            <td>11</td>
-            <td>firebase - firebase tool</td>
-          </tr>
-        </tbody>
-      </Table>
-
+    {/* comment the below div or delete and work */}
+      <div>
+        <h1>React Pack</h1>
+        <p> -by chaudhuree</p>
+        <br />
+        <br />
+        <br />
+        <hr />
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Number</th>
+              <th>Package name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>redux</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>react-redux</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>axios</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>graphql</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>apollo client</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>bootstrap</td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td>react-router-dom</td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td>styled component</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>redux toolkit</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>recoil</td>
+            </tr>
+            <tr>
+              <td>11</td>
+              <td>firebase - firebase tool</td>
+            </tr>
+            <tr>
+              <td>12</td>
+              <td>node-sass</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     </div>
 
   );
